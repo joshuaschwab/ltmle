e1a37f46ab646cc46f8511aa96291fb797b643bf
Joshua Schwab <joshuaschwab@yahoo.com>
2016-11-18 09:39:06 -0800

    accepted on CRAN v0.9-9

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 release_questions <- function() {
-  c('Have you run check coverage.R (in /ltmle development)', 'Have you moved test-prevRelease.R back to /ltmle development?', 'Have you run "create tests to compare versions.R" (in /ltmle development)?', 'Have you run CheckBeforeSubmit()?  (in utils/GeneralUtilities.R)') # nocov
+  c('Have you updated the R version in cran-comments.rd', 'Have you run check coverage.R (in /ltmle development)', 'Have you moved test-prevRelease.R back to /ltmle development?', 'Have you run "create tests to compare versions.R" (in /ltmle development)?', 'Have you run CheckBeforeSubmit()?  (in utils/GeneralUtilities.R)') # nocov
 }
--- cran-comments.md
+++ cran-comments.md
@@ -1,18 +1,11 @@
 ## Test environments
-* local OS X install, R 3.2.3
+* local OS X install, R 3.3.2
 * win-builder (devel and release)
 
 ## R CMD check results
 There were no ERRORs or WARNINGs. There is one NOTE:  
   Maintainer: Joshua Schwab <joshuaschwab@yahoo.com>
   (this is my correct email)
-  
-  
-This NOTE has been fixed, sorry about that:
-* checking top-level files ... NOTE
-Non-standard file/directory found at top level:
-  'revdep'
-
 
 ## Downstream dependencies
 I have run devtools::revdep_check on ltmle. There is 1 downstream dependency and no failures were reported.
